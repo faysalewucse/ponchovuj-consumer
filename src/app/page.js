f03a1a80d287dd49db892ee13d374e369b892a52
shamim26 <shamimreja101@gmail.com>
2024-03-20 13:39:25 +0600

    top second nav added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import { TopNavbar } from "@/components/top-nav/TopNavbar";
+import { TopSecondNavbar } from "@/components/top-second-nav/TopSecondNavbar";
 
 export default function Home() {
   return (
     <main>
-      <div className="border-b">
+      <div className="border-b hidden md:block">
         <TopNavbar />
       </div>
+      <TopSecondNavbar />
     </main>
   );
 }
